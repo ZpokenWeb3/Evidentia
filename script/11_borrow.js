87220b930a3c8e884f5d6bf40dd458a214284446
Dmytro Biriukov <dimbirua@gmail.com>
2024-09-19 16:38:28 +0300

    script: borrow

---
+++ script/11_borrow.js
@@ -0,0 +1,35 @@
+const { ethers } = require('ethers');
+const fs = require('fs');
+require('dotenv').config();
+
+const contractAddress = '0x049fCAB83597C4E6dC2331D945736A3009AE60B7'; // NftStaking
+const contractABIPath = './script/ABI/NFTStakingAndBorrowing.json';
+
+const privateKey = process.env.PRIVATE_KEY;
+const rpcUrl = process.env.SEPOLIA_RPC_URL;
+
+const contractABI = JSON.parse(fs.readFileSync(contractABIPath, 'utf8'));
+
+async function borrow() {
+  try {
+    const provider = new ethers.JsonRpcProvider(rpcUrl);
+    const wallet = new ethers.Wallet(privateKey, provider);
+
+    const contract = new ethers.Contract(contractAddress, contractABI, wallet);
+
+    console.log('Borrowing stables...');
+    const amount = 1000_000000;
+    const tx = await contract.borrow(amount);
+    console.log(`Transaction hash: ${tx.hash}`);
+
+    const receipt = await tx.wait();
+    console.log('Transaction confirmed');
+    console.log(`Gas used: ${receipt.gasUsed.toString()}`);
+    console.log(`Block number: ${receipt.blockNumber}`);
+
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+}
+
+borrow();
