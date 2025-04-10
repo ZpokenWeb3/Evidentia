25f1f8ea10e62a650f1cd057e40c1e275981c481
Dmytro Biriukov <dimbirua@gmail.com>
2025-04-10 16:39:15 +0300

    fuzzy tests for borrow and debt added

--- test/TestLog.t.sol
+++ test/TestLog.t.sol
@@ -31,14 +31,30 @@ contract TestLogTest is Test {
         uint256 x256 = (uint256(x) + uint256(2)) * 1e18;
         uint256 maxBorrow = testlog.calculateMaxBorrow(x256, START_TIME, START_TIME + YEAR_IN_SECONDS);
         uint256 diff = 0;
-        if ((x256 - maxBorrow * PROTOCOL_YIELD / DECIMALS_MULTIPLIER) >= maxBorrow) {
-            diff = (x256 - maxBorrow * PROTOCOL_YIELD / DECIMALS_MULTIPLIER) - maxBorrow;
+        uint256 borrow_debt = maxBorrow * PROTOCOL_YIELD / DECIMALS_MULTIPLIER;
+        if ((x256 - borrow_debt) >= maxBorrow) {
+            diff = (x256 - borrow_debt) - maxBorrow;
         } else {
-            diff = maxBorrow - (x256 - maxBorrow * PROTOCOL_YIELD / DECIMALS_MULTIPLIER);
+            diff = maxBorrow - (x256 - borrow_debt);
         }
-        
-        assertGt(maxBorrow/1e17, diff);
-        
+
+        assertGt(maxBorrow / 5e16, diff);
+    }
+
+    function testFuzz_MaxBorrow_2years(uint128 x) public view {
+        uint256 x256 = (uint256(x) + uint256(2)) * 1e18;
+        uint256 maxBorrow = testlog.calculateMaxBorrow(x256, START_TIME, START_TIME + 2 * YEAR_IN_SECONDS);
+        uint256 diff = 0;
+        uint256 first_year_debt = maxBorrow * PROTOCOL_YIELD / DECIMALS_MULTIPLIER;
+        uint256 second_year_debt = (first_year_debt + maxBorrow) * PROTOCOL_YIELD / DECIMALS_MULTIPLIER;
+
+        if ((x256 - first_year_debt - second_year_debt) >= maxBorrow) {
+            diff = (x256 - first_year_debt - second_year_debt) - maxBorrow;
+        } else {
+            diff = maxBorrow - (x256 - first_year_debt - second_year_debt);
+        }
+
+        assertGt(maxBorrow / 2e16, diff);
     }
 
     function testFuzz_Debt(uint128 x) public view {
@@ -50,8 +66,90 @@ contract TestLogTest is Test {
         } else {
             diff = debt - (x256 + x256 * PROTOCOL_YIELD / DECIMALS_MULTIPLIER);
         }
-        
-        assertGt(debt/2e16, diff);
+
+        assertGt(debt / 2e16, diff);
     }
 
+    function testFuzz_Debt_2years(uint128 x) public view {
+        uint256 x256 = (uint256(x) + uint256(2)) * 1e18;
+        uint256 debt = testlog.calculateDebt(x256, START_TIME, START_TIME + 2 * YEAR_IN_SECONDS);
+        uint256 diff = 0;
+        uint256 first_year_debt = x256 + x256 * PROTOCOL_YIELD / DECIMALS_MULTIPLIER;
+        uint256 second_year_debt = first_year_debt + first_year_debt * PROTOCOL_YIELD / DECIMALS_MULTIPLIER;
+        if (second_year_debt >= debt) {
+            diff = second_year_debt - debt;
+        } else {
+            diff = debt - second_year_debt;
+        }
+
+        assertGt(debt / 2e16, diff);
+    }
+
+    function testFuzz_MaxBorrow_1month(uint128 x) public view {
+        uint256 x256 = (uint256(x) + uint256(2)) * 1e18;
+        uint256 month_in_seconds = YEAR_IN_SECONDS / 12;
+        uint256 month_yield = 948879293458 * DECIMALS_MULTIPLIER / (BIPS * 1e10); // (1.12)**(1/12)
+        uint256 maxBorrow = testlog.calculateMaxBorrow(x256, START_TIME, START_TIME + month_in_seconds);
+        uint256 diff = 0;
+        uint256 month_debt = maxBorrow * month_yield / DECIMALS_MULTIPLIER;
+
+        if ((x256 - month_debt) >= maxBorrow) {
+            diff = (x256 - month_debt) - maxBorrow;
+        } else {
+            diff = maxBorrow - (x256 - month_debt);
+        }
+
+        assertGt(maxBorrow / 3e14, diff);
+    }
+
+    function testFuzz_MaxBorrow_2months(uint128 x) public view {
+        uint256 x256 = (uint256(x) + uint256(2)) * 1e18;
+        uint256 month_in_seconds = YEAR_IN_SECONDS / 12;
+        uint256 month_yield = 948879293458 * DECIMALS_MULTIPLIER / (BIPS * 1e10); // (1.12)**(1/12)
+        uint256 maxBorrow = testlog.calculateMaxBorrow(x256, START_TIME, START_TIME + 2 * month_in_seconds);
+        uint256 diff = 0;
+        uint256 first_month_debt = maxBorrow * month_yield / DECIMALS_MULTIPLIER;
+        uint256 second_month_debt = (maxBorrow + first_month_debt) * month_yield / DECIMALS_MULTIPLIER;
+
+        if ((x256 - first_month_debt - second_month_debt) >= maxBorrow) {
+            diff = (x256 - first_month_debt - second_month_debt) - maxBorrow;
+        } else {
+            diff = maxBorrow - (x256 - first_month_debt - second_month_debt);
+        }
+
+        assertGt(maxBorrow / 1e14, diff);
+    }
+
+    function testFuzz_Debt_1month(uint128 x) public view {
+        uint256 x256 = (uint256(x) + uint256(2)) * 1e18;
+        uint256 month_in_seconds = YEAR_IN_SECONDS / 12;
+        uint256 month_yield = 948879293458 * DECIMALS_MULTIPLIER / (BIPS * 1e10); // (1.12)**(1/12)
+        uint256 debt = testlog.calculateDebt(x256, START_TIME, START_TIME + month_in_seconds);
+        uint256 diff = 0;
+        uint256 first_month_debt = x256 + x256 * month_yield / DECIMALS_MULTIPLIER;
+        if (first_month_debt >= debt) {
+            diff = first_month_debt - debt;
+        } else {
+            diff = debt - first_month_debt;
+        }
+
+        assertGt(debt / 1e14, diff);
+    }
+
+    function testFuzz_Debt_2month(uint128 x) public view {
+        uint256 x256 = (uint256(x) + uint256(2)) * 1e18;
+        uint256 month_in_seconds = YEAR_IN_SECONDS / 12;
+        uint256 month_yield = 948879293458 * DECIMALS_MULTIPLIER / (BIPS * 1e10); // (1.12)**(1/12)
+        uint256 debt = testlog.calculateDebt(x256, START_TIME, START_TIME + 2 * month_in_seconds);
+        uint256 diff = 0;
+        uint256 first_month_debt = x256 + x256 * month_yield / DECIMALS_MULTIPLIER;
+        uint256 second_month_debt = first_month_debt + first_month_debt * month_yield / DECIMALS_MULTIPLIER;
+        if (second_month_debt >= debt) {
+            diff = second_month_debt - debt;
+        } else {
+            diff = debt - second_month_debt;
+        }
+
+        assertGt(debt / 1e14, diff);
+    }
 }
