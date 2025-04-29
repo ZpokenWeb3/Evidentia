d25c190e927a3186134d6e84cfbaa0927c573114
valentine <valentyn1789@gmail.com>
2025-04-29 18:09:29 +0300

    rebase on master

--- script/04_DeployStablesStaking.s.sol
+++ script/04_DeployStablesStaking.s.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.22;
+pragma solidity 0.8.22;
 
 import {Script} from "forge-std/Script.sol";
 import {StableCoinsStaking} from "../src/StableCoinsStaking.sol";
 import {console} from "forge-std/console.sol";
 
-contract DeployStablesStaking is Script {
-    function run() external returns (StableCoinsStaking) {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        // address stableCoinsAddress = vm.envAddress("STABLES_ADDRESS");
-        // address nftStakingAddress = vm.envAddress("NFT_STAKING_ADDRESS");
-        vm.startBroadcast(deployerPrivateKey);
-        // StableCoinsStaking stableStaking = new StableCoinsStaking();
-        // stableStaking.initialize(address(stableBondCoins), address(nftStaking), address(owner));
-        vm.stopBroadcast();
-        // return stablesStaking;
-    }
-}
+// contract DeployStablesStaking is Script {
+//     function run() external returns (StableCoinsStaking) {
+//         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+//         address stableCoinsAddress = vm.envAddress("STABLES_ADDRESS");
+//         address nftStakingAddress = vm.envAddress("NFT_STAKING_ADDRESS");
+//         vm.startBroadcast(deployerPrivateKey);
+//         // StableCoinsStaking stableStaking = new StableCoinsStaking();
+//         // stableStaking.initialize(address(stableBondCoins), address(nftStaking), address(owner));
+//         vm.stopBroadcast();
+//         // return stablesStaking;
+//     }
+// }
