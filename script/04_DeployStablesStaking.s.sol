bde9e4b34e9f383864276ce4e3a032bdd1f2cee9
zpsmcdev <115453065+zpsmcdev@users.noreply.github.com>
2025-04-30 01:45:48 +0300

    Fix DeployStablesStaking script

--- script/04_DeployStablesStaking.s.sol
+++ script/04_DeployStablesStaking.s.sol
@@ -4,16 +4,27 @@ pragma solidity 0.8.22;
 import {Script} from "forge-std/Script.sol";
 import {StableCoinsStaking} from "../src/StableCoinsStaking.sol";
 import {console} from "forge-std/console.sol";
+import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
 contract DeployStablesStaking is Script {
     function run() external returns (StableCoinsStaking) {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         address stableCoinsAddress = vm.envAddress("STABLES_ADDRESS");
         address nftStakingAddress = vm.envAddress("NFT_STAKING_ADDRESS");
+        address owner = vm.envAddress("OWNER_ADDRESS");
+
         vm.startBroadcast(deployerPrivateKey);
-        // StableCoinsStaking stableStaking = new StableCoinsStaking();
-        // stableStaking.initialize(address(stableBondCoins), address(nftStaking), address(owner));
+
+        address proxy = Upgrades.deployUUPSProxy(
+            "StableCoinsStaking.sol",
+            abi.encodeCall(StableCoinsStaking.initialize, (stableCoinsAddress, nftStakingAddress, owner))
+        );
+
+        StableCoinsStaking stableStaking = StableCoinsStaking(proxy);
+
         vm.stopBroadcast();
-        // return stablesStaking;
+
+        console.log("StableCoinsStaking deployed at:", address(stableStaking));
+        return stableStaking;
     }
 }
