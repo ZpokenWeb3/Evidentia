5b700c77825dd52b2f70f732f7312b55b4a9c583
Dmytro Biriukov <dimbirua@gmail.com>
2025-04-29 17:41:15 +0300

    skip deploy script

--- script/04_DeployStablesStaking.s.sol
+++ script/04_DeployStablesStaking.s.sol
@@ -5,15 +5,15 @@ import {Script} from "forge-std/Script.sol";
 import {StableCoinsStaking} from "../src/StableCoinsStaking.sol";
 import {console} from "forge-std/console.sol";
 
-contract DeployStablesStaking is Script {
-    function run() external returns (StableCoinsStaking) {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        address stableCoinsAddress = vm.envAddress("STABLES_ADDRESS");
-        address nftStakingAddress = vm.envAddress("NFT_STAKING_ADDRESS");
-        vm.startBroadcast(deployerPrivateKey);
-        // StableCoinsStaking stableStaking = new StableCoinsStaking();
-        // stableStaking.initialize(address(stableBondCoins), address(nftStaking), address(owner));
-        vm.stopBroadcast();
-        // return stablesStaking;
-    }
-}
+// contract DeployStablesStaking is Script {
+//     function run() external returns (StableCoinsStaking) {
+//         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+//         address stableCoinsAddress = vm.envAddress("STABLES_ADDRESS");
+//         address nftStakingAddress = vm.envAddress("NFT_STAKING_ADDRESS");
+//         vm.startBroadcast(deployerPrivateKey);
+//         // StableCoinsStaking stableStaking = new StableCoinsStaking();
+//         // stableStaking.initialize(address(stableBondCoins), address(nftStaking), address(owner));
+//         vm.stopBroadcast();
+//         // return stablesStaking;
+//     }
+// }
